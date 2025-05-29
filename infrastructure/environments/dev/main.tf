e8bb37f36ad9fcf621f0dabfb2cc4630a8b9f9ef
Paul O'Brien <6592415+p-obrien@users.noreply.github.com>
2025-05-29 17:57:11 +1000

    chore: parameterize more variables

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -3,9 +3,9 @@ module "vpc" {
 
   vpc_name        = var.vpc_name
   vpc_cidr_block  = var.vpc_cidr_block
-  azs             = ["ap-southeast-2a", "ap-southeast-2b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
 }
 
